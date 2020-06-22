e827951b671f2b30229efcdcb9708336b1131d48
Jignasha Desai <desaijignasha2009@gmail.com>
2020-06-22 14:45:23 +0530

    Initial Commit

---
+++ AzureKickStart.Common/StorageService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System;
+using System.Threading.Tasks;
+
+namespace AzureKickStart.Common
+{
+    public class StorageService
+    {
+        private readonly CloudBlobClient blobClient;
+
+        public StorageService(string storageConnectionString)
+        {
+            var mycloudStorageAccount = CloudStorageAccount.Parse(storageConnectionString);
+            blobClient = mycloudStorageAccount.CreateCloudBlobClient();
+        }
+
+        public async Task<Byte[]> DownloadBlob(string containerName, string file)
+        {
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(file);
+
+            await blockBlob.FetchAttributesAsync();
+            long fileByteLength = blockBlob.Properties.Length;
+            byte[] fileContent = new byte[fileByteLength];
+            for (int i = 0; i < fileByteLength; i++)
+            {
+                fileContent[i] = 0x20;
+            }
+            await blockBlob.DownloadToByteArrayAsync(fileContent, 0);
+            return fileContent;
+        }
+
+        public async Task<string> UploadImageToAzureBlobStorageAsync(string containerName, string blobName, byte[] bytes, string contentType)
+        {
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            var blockBlob = container.GetBlockBlobReference(blobName);
+
+            blockBlob.Properties.ContentType = contentType;
+
+            await blockBlob.UploadFromByteArrayAsync(bytes, 0, bytes.Length);
+
+            return blockBlob.Uri.AbsoluteUri;
+        }
+    }
+}
