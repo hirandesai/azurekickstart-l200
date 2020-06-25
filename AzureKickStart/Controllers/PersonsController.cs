17e62b721fc81588b27ab6e5727fb5d7d0bea119
Jignasha Desai <desaijignasha2009@gmail.com>
2020-06-25 11:55:16 +0530

    Removed unnecessary code.

--- AzureKickStart/Controllers/PersonsController.cs
+++ AzureKickStart/Controllers/PersonsController.cs
@@ -1,13 +1,10 @@
+using AzureKickStart.Models;
 using System;
-using System.Collections.Generic;
-using System.Data;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using AzureKickStart.Models;
-using System.Diagnostics;
 
 namespace AzureKickStart.Controllers
 {
